c1ebcd02a88780aa2b0b4ff0fabe313fedbe1e61
gokulramesh94 <gokulpulari@gmail.com>
2021-06-15 03:11:30 -0400

    created test case project.

---
+++ GokulAssignment2.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GokulAssignment2.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GokulAssignment2.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3e3fa079-6298-4d66-a51f-24bcfc8a4ff4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ GokulAssignment2.Tests/RectangleAppTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace GokulAssignment2.Tests
+{
+    [TestClass]
+    public class RectangleAppTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
